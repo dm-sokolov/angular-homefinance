541d402e1d107f4d4cde7207effbdd22060375e3
Dmitriy Sokolov <sda.falcon@gmail.com>
2021-04-24 23:23:23 +0500

    init

--- HomeFinance-UI/e2e/src/app.e2e-spec.ts
+++ HomeFinance-UI/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('HomeFinanceWeb app is running!');
+    expect(await page.getTitleText()).toEqual('HomeFinance-UI app is running!');
   });
 
   afterEach(async () => {
--- HomeFinance-UI/karma.conf.js
+++ HomeFinance-UI/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/HomeFinanceWeb'),
+      dir: require('path').join(__dirname, './coverage/HomeFinance-UI'),
       subdir: '.',
       reporters: [
         { type: 'html' },
