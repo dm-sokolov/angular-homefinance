91a576a483048696707eee77b8c913188234587e
Dmitriy Sokolov <sda.falcon@gmail.com>
2021-04-29 00:28:33 +0500

    обработка ошибок

--- HomeFinance-UI/src/app/admin/login-page/login-page.component.ts
+++ HomeFinance-UI/src/app/admin/login-page/login-page.component.ts
@@ -12,9 +12,10 @@ import {Router} from "@angular/router";
 export class LoginPageComponent implements OnInit {
 
   form: FormGroup;
+  isSubmitted = false;
 
   constructor(
-    private auth: AuthService,
+    public auth: AuthService,
     private router: Router
   ) { }
 
@@ -36,6 +37,8 @@ export class LoginPageComponent implements OnInit {
     if (this.form.invalid)
       return;
 
+    this.isSubmitted = true;
+
     const user: User = {
       email: this.form.value.email,
       password: this.form.value.password
@@ -44,6 +47,9 @@ export class LoginPageComponent implements OnInit {
     this.auth.login(user).subscribe(() => {
       this.form.reset()
       this.router.navigate(['/admin', 'dashboard'])
+      this.isSubmitted = false
+    }, () => {
+      this.isSubmitted = false
     })
   }
 }
